ca68a5990f530c057c4d73560341dc8462efe14e
energetiq <energetiq@outlook.com>
2024-01-14 20:13:27 +0100

    Refactor code to calculate percentage results in versionweb.c

--- versionweb.c
+++ versionweb.c
@@ -20,6 +20,18 @@ void lecture_style()
   }
 }
 void versionweb() {
+    int presultsjan=results().jan*100;
+    int presultsfeb=results().feb*100;
+    int presultsmar=results().mar*100;
+    int presultsapr=results().apr*100;
+    int presultsmay=results().may*100;
+    int presultsjun=results().jun*100;
+    int presultsjul=results().jul*100;
+    int presultsaug=results().aug*100;
+    int presultssep=results().sep*100;
+    int presultsoct=results().oct*100;
+    int presultsnov=results().nov*100;
+    int presultsdec=results().dec*100;
 
     printf("Content-type: text/html\n\n");
 
@@ -35,50 +47,50 @@ void versionweb() {
     printf("    <ul>\n");
 
     printf("        <li style=\"left: 10px; height : %d", results().jan/100);
-    printf("px;\">%d", results().jan/1000);
+    printf("px;\">%d", presultsjan/results().total);
     printf("</li>\n");
 
     printf("        <li style=\"left: 41px; height : %d", results().feb/100);
-    printf("px;\">%d", results().feb/1000);
+    printf("px;\">%d", presultsfeb/results().total);
     printf("</li>\n");
 
     printf("        <li style=\"left: 72px; height : %d", results().mar/100);
-    printf("px;\">%d", results().mar/1000);
+    printf("px;\">%d", presultsmar/results().total);
     printf("</li>\n");
     printf("        <li style=\"left: 103px; height : %d", results().apr/100);
-    printf("px;\">%d", results().apr/1000);
+    printf("px;\">%d", presultsapr/results().total);
     printf("</li>\n");
 
     printf("        <li style=\"left: 134px; height : %d", results().may/100);
-    printf("px;\">%d", results().may/1000);
+    printf("px;\">%d", presultsmay/results().total);
     printf("</li>\n");
 
     printf("        <li style=\"left: 165px; height : %d", results().jun/100);
-    printf("px;\">%d", results().jun/1000);
+    printf("px;\">%d", presultsjun/results().total);
     printf("</li>\n");
 
     printf("        <li style=\"left: 196px; height : %d", results().jul/100);
-    printf("px;\">%d", results().jul/1000);
+    printf("px;\">%d", presultsjul/results().total);
     printf("</li>\n");
 
     printf("        <li style=\"left: 227px; height : %d", results().aug/100);
-    printf("px;\">%d", results().aug/1000);
+    printf("px;\">%d", presultsaug/results().total);
     printf("</li>\n");
 
     printf("        <li style=\"left: 258px; height : %d", results().sep/100);
-    printf("px;\">%d", results().sep/1000);
+    printf("px;\">%d", presultssep/results().total);
     printf("</li>\n");
 
     printf("        <li style=\"left: 289px; height : %d", results().oct/100);
-    printf("px;\">%d", results().oct/1000);
+    printf("px;\">%d", presultsoct/results().total);
     printf("</li>\n");
 
     printf("        <li style=\"left: 320px; height : %d", results().nov/100);
-    printf("px;\">%d", results().nov/1000);
+    printf("px;\">%d", presultsnov/results().total);
     printf("</li>\n");
 
     printf("        <li style=\"left: 351px; height : %d", results().dec/100);
-    printf("px;\">%d", results().dec/100);
+    printf("px;\">%d", presultsdec/results().total);
     printf("</li>\n");
 
     printf("    </ul>\n");
